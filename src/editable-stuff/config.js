654a1de264da93f5cbe0fef3346d01e0beaf9e0b
Stephen Anino <aninostephen01@gmail.com>
2024-01-10 11:33:29 +0800

    first commit

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -5,7 +5,7 @@ const navBar = {
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f",
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6",
   firstName: "Stephen",
   middleName: "",
   lastName: "Anino",
